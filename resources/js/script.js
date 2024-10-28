fc7f114d35b5da76eaf65d5fab2bf9989a631bcb
Energy0120 <omar191@hotmail.it>
2024-10-28 20:33:25 +0100

    Moved scripts to a separate file, made the buttons functional and somewhat viewable in smaller screens.

---
+++ resources/js/script.js
@@ -0,0 +1,25 @@
+const buttons = document.querySelectorAll(".toggle-buttons");
+const paragraphs = document.querySelectorAll(".textparagraphs > div");
+
+let saveVal = paragraphs[0];
+
+for (let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener("click", function(){
+        hideandShow(i);
+    })
+
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function hideandShow(index){
+    paragraphs[index].style.opacity = "0";
+    paragraphs[index].style.display = "block";
+    saveVal.style.display = "none";
+    sleep(100).then(() => {
+        paragraphs[index].style.opacity = "1";
+        saveVal = paragraphs[index];
+    });
+}
